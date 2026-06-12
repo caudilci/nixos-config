e4a2da983e120ed86bd7de0bf1fe71eebb3316d9
Christopher Caudill <57081300+caudilci@users.noreply.github.com>
2026-06-12 15:43:07 -0400

    greeter

--- modules/core.nix
+++ modules/core.nix
@@ -1,4 +1,5 @@
 { config, pkgs, ... }: {
+  # --- System Basics ---
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   boot.loader.systemd-boot.enable = true;
@@ -21,12 +22,33 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  # --- User Identity ---
   users.users.cc = {
     isNormalUser = true;
     description = "cc";
     extraGroups = [ "networkmanager" "wheel" "docker" "video" "render" ];
   };
 
+  # --- Graphical Login (Greetd + QtGreetD) ---
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        # This launches the qtgreetd binary
+        cmd = "${pkgs.qtgreetd}/bin/qtgreetd";
+        user = "greeter";
+      };
+    };
+  };
+
+  # QtGreetD requires a configuration file to know what to launch after login
+  environment.etc."qtgreetd".text = ''
+    # This tells the greeter to start Niri with a DBus session
+    # so that Noctalia and other apps work correctly.
+    command = "dbus-run-session niri"
+  '';
+
+  # --- System Services & Permissions ---
   virtualisation.docker.enable = true;
   nixpkgs.config.allowUnfree = true;
 }

0c69dedb0742e6112ee71676cd5676b3706649bf
= <=>
2026-06-12 14:03:31 -0400

    update flake to fix overlays location and github link

--- flake.nix
+++ flake.nix
@@ -3,33 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     niri.url = "github:niri-wm/niri";
-
-    amd-ai = {
-      url = "git+file:///home/cc/nix-amd-ai";
-      flake = true;
-    };
+    amd-ai.url = "github:noamsto/nix-amd-ai";
   };
 
   outputs = { self, nixpkgs, niri, amd-ai, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
-      overlays = [
-        amd-ai.overlays.default
-        # The official flake provides an overlay for the niri package
-        niri.overlays.default
-      ];
-
       modules = [
+        {
+          nixpkgs.overlays = [
+            amd-ai.overlays.default
+            niri.overlays.default
+          ];
+        }
+
         ./hosts/nixos/default.nix
         ./modules/core.nix
         ./modules/graphics.nix
         ./modules/desktop.nix
         ./modules/ai-stack.nix
         amd-ai.nixosModules.default
-        # The official flake provides the NixOS module here
         niri.nixosModules.niri
       ];
     };

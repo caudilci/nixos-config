a6aa9639a3fefa1fb406b3e52ef039a80ee569f2
Chris Caudill <christopheriancaudill@gmail.com>
2026-06-18 23:27:44 -0400

    remove niri

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, niri, amd-ai, ... }: {
+  outputs = inputs@{ self, nixpkgs, amd-ai, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
